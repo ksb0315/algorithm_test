77c23e3c0e5c35ec622e2de1d6d92cf36628b607
Sunbum Kwon <ksunbum97@gmail.com>
2022-01-16 18:28:32 +0900

    third commit

---
+++ 백준/project6.py
@@ -0,0 +1,29 @@
+# N(1 ≤ N ≤ 100,000)개의 로프가 있다. 이 로프를 이용하여 이런 저런 물체를 들어올릴 수 있다. 각각의 로프는 그 굵기나 길이가 다르기 때문에 들 수 있는 물체의 중량이 서로 다를 수도 있다.
+# 하지만 여러 개의 로프를 병렬로 연결하면 각각의 로프에 걸리는 중량을 나눌 수 있다. k개의 로프를 사용하여 중량이 w인 물체를 들어올릴 때, 각각의 로프에는 모두 고르게 w/k 만큼의 중량이 걸리게 된다.
+# 각 로프들에 대한 정보가 주어졌을 때, 이 로프들을 이용하여 들어올릴 수 있는 물체의 최대 중량을 구해내는 프로그램을 작성하시오. 모든 로프를 사용해야 할 필요는 없으며, 임의로 몇 개의 로프를 골라서 사용해도 된다.
+
+# n = int(input())
+# k = []
+# num = 0
+# for i in range(n):
+#     num = int(input())
+#     k.append(num)
+# k.sort()
+# result = k[0] * n
+# print(result)
+
+def solution():
+    answer = 0
+    arr.sort(reverse=True)
+    for i in range(N):
+        arr[i] = arr[i] * (i + 1)
+ 
+    return max(arr)
+ 
+ 
+N = int(input())
+arr = []
+for _ in range(N):
+    arr.append(int(input()))
+ 
+print(solution())
