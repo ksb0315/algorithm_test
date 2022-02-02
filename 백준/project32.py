dd5f05c0f3bf99f983b8ef5ef48dcc39dc526230
Sunbum Kwon <ksunbum97@gmail.com>
2022-02-02 21:15:59 +0900

    ten fifth

---
+++ 백준/project32.py
@@ -0,0 +1,13 @@
+# N자리 숫자가 주어졌을 때, 여기서 숫자 K개를 지워서 얻을 수 있는 가장 큰 수를 구하는 프로그램을 작성하시오.
+
+N, K = map(int, input().split())
+num = list(input())
+k, stack = K, []
+
+for i in range(N):
+    while k > 0 and stack and stack[-1] < num[i]:
+        stack.pop()
+        k-=1
+    stack.append(num[i])
+
+print(''.join(stack[:N-K]))
