1b290577c442e2fc5cfe89311f06a99563529278
Sunbum Kwon <ksunbum97@gmail.com>
2022-03-15 15:05:40 +0900

    new11

--- 백준/1036.py
+++ 백준/1036.py
@@ -1,38 +1,41 @@
 # 36진법의 숫자는 0부터 9까지의 수와 알파벳 A에서 Z로 나타낸다. A부터 Z까지 알파벳은 10부터 35에 차례대로 대응한다.
 # 36진법의 수 N개가 주어진다. 36진법 숫자(0-9, A-Z) 중에서 K개의 숫자를 고른다. 그러고 나서 N개의 수 모두에서 나타난 그 숫자를 Z로 바꾼다. 그 이후에 N개의 수를 모두 더한다.
 # 이때 가능한 합의 최댓값을 구하는 프로그램을 작성하시오. 합의 최댓값도 36진수로 출력한다.
-def f(n):
-    q = n // 36
-    r = n % 36
-    if q:
-        if 0<=r<=9:
-            return f(q) + chr(r+48) 
-        else:
-            return f(q) + chr(r+55)
+import sys
+
+def to_36(N):
+    d = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    a, b = N // 36, N % 36
+    w =d[b]
+    if a!=0:
+        return to_36(a) + w  
     else:
-        if 0<=r<=9:
-            chr(r+48) 
-        else:
-            chr(r+55)
+        return w
 
-
-    return f(q) + (chr(r+48) if 0 <= r <= 9 else chr(r+55)) if q else chr(r+48) if 0<= r <=9 else chr(r+55)
 N = int(input())
-c = [0]*36
-for i in range(N):
-    n = input()
-    l = len(n)
-    for j in range(l):
-        c[int(n[j],36)] += 36**(l-j-1)
-K = int(input())
-A = [[c[i]*(35-i), i]for i in range(36)]
-A.sort()
-s=0
-for i in range(36):
-    if i < 36-K:
-        s+=c[A[i][1]] * A[i][1] 
-    else:
-        s+=c[A[i][1]] * 35   
-    
-print(f(s))
+count=dict()
+for i in list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+    count[i]=0
+digit = []
+for _ in range(N):
+    digit.append(sys.stdin.readline().strip())
 
+K=int(input())
+
+for i in digit:
+    for j in range(len(i)):
+        count[i[::-1][j]] +=36**j
+    
+for i in count.keys():
+    count[i] = count[i] * (35-int(i, 36))
+count = sorted(count.items())
+count.sort(key=lambda x: -x[1])
+
+for i in count[:K]:
+    for j in range(len(digit)):
+        digit[j] = digit[j].replace(i[0],"Z")    
+s=0
+for i in digit:
+    s += int(i, 36)
+
+print(to_36(s))
--- 백준/test.py
+++ 백준/test.py
@@ -1,42 +1,14 @@
-import sys
+n, m = map(int, input().split())
 
+def count_number(n, k):
+    count = 0
+    while n:
+        n //= k
+        count += n
+    return count
 
-def solve():
-    N = int(input())
-    count=dict()
-    for i in list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-        count[i]=0
-    n_list = list()
-    for _ in range(N):
-        n_list.append(sys.stdin.readline().strip())
-    
-    K=int(input())
+five = count_number(n, 5) - count_number(m, 5) - count_number(n-m, 5)
+two = count_number(n, 2) - count_number(m, 2) - count_number(n-m, 2)
 
-    for i in n_list:
-        for j in range(len(i)):
-            count[i[::-1][j]] +=36**j
-    
-    for i in count.keys():
-        count[i] = count[i] * (35-int(i, 36))
-    count = sorted(count.items())
-    count.sort(key=lambda x: -x[1])
-
-    for i in count[:K]:
-        for j in range(len(n_list)):
-            n_list[j] = n_list[j].replace(i[0],"Z")    
-    s=0
-    for i in n_list:
-        s += int(i, 36)
-
-    print(to_36(s))
-
-def to_36(N):
-    d = list("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    a, b = N // 36, N % 36
-    w =d[b]
-    if a!=0:
-        return to_36(a) + w  
-    else:
-        return w
-    
-solve()
+ans = min(five, two)
+print(ans)
