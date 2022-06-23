f8706409d91e97ad081a881dbe22505b8594cd67
Sunbum Kwon <ksunbum97@gmail.com>
2022-06-23 14:04:52 +0900

    new29

---
+++ 백준/1181.py
@@ -0,0 +1,14 @@
+# 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+# 길이가 짧은 것부터
+# 길이가 같으면 사전 순으로
+
+n = int(input())
+words = []
+for _ in range(n):
+    words.append(input())
+temp = set(words)
+words = sorted(list(temp))
+words = sorted(words, key=lambda x : len(x))
+
+for word in words:
+    print(word)
