0110a4ec52320f397e18bbd0fb68eac44d39ba33
Sunbum Kwon <ksunbum97@gmail.com>
2022-06-24 15:10:54 +0900

    new30

---
+++ 백준/1149.py
@@ -0,0 +1,22 @@
+# RGB거리에는 집이 N개 있다. 거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다.
+# 집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다. 각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때, 아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자.
+#    1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+#    N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+#    i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+RGB = []
+for i in range(n):
+    RGB.append(list(map(int, input().strip().split())))
+
+for i in range(1, n):
+    #빨간집
+    RGB[i][0] = min(RGB[i - 1][1], RGB[i - 1][2]) + RGB[i][0]
+    #초록집
+    RGB[i][1] = min(RGB[i - 1][0], RGB[i - 1][2]) + RGB[i][1]
+    #파란집
+    RGB[i][2] = min(RGB[i - 1][0], RGB[i - 1][1]) + RGB[i][2]
+print(min(RGB[n - 1]))
--- 백준/1912.py
+++ 백준/1912.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 n = int(input())
 arr = list(map(int,input().split()))
 
-for i in range(len(arr)):
+for i in range(1, n):
     arr[i] = max(arr[i], arr[i-1]+arr[i])
 
 print(max(arr))
